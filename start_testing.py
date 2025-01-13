5ae9aceeb87ca17d6ebf7c224899971f486218f5
noc-turne <2270929247@qq.com>
2025-01-13 16:47:22 +0800

    change testing

--- start_testing.py
+++ start_testing.py
@@ -7,6 +7,7 @@ import time
 from datetime import datetime
 import pandas as pd
 import aiohttp
+import sys
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
@@ -14,67 +15,13 @@ logging.basicConfig(
 current_file = os.path.splitext(os.path.basename(__file__))[0]
 logger = logging.getLogger(current_file)
 
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 
-# GPU RELATED
-def gpu_info2txt(file_name, response):
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    with open(file_name, "a") as file:
-        file.write(f"Current Time: {timestamp}\n")
-        for gpu in response:
-            file.write(f"GPU {gpu['gpu_id']} ({gpu['name']}):\n")
-            file.write(f"  GPU Utilization: {gpu['gpu_utilization']}%\n")
-            file.write(f"  Memory Utilization: {gpu['memory_utilization']}%\n")
-            file.write(f"  Memory: {gpu['memory_used']} MiB / {gpu['memory_total']} MiB\n")
-            file.write("\n")
-        file.write("\n")
-
-
-async def fetch_gpu_info(api_url):
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(api_url) as response:
-                if response.status == 200:
-                    return await response.json()
-                else:
-                    logger.error(f"Received status code {response.status} for URL {api_url}")
-                    return None
-        except Exception as e:
-            logger.error(f"Failed to fetch GPU info from {api_url}. Exception: {e}")
-            return None
-
-
-async def monitor_gpu(api_url, interval, file_name, stop_event):
-    while not stop_event.is_set():
-        response = await fetch_gpu_info(api_url)
-        if response:  # Ensure the response is valid
-            gpu_info2txt(file_name, response)
-        await asyncio.sleep(interval)
-
-
-async def gpu_main(models, save_path, stop_event):
-    tasks = []
-    gpu_info_path = os.path.join(save_path, "gpu_info")
-    os.makedirs(gpu_info_path, exist_ok=True)
-    for idx, model in enumerate(models):
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S") + f"_{idx}"
-        if "gpu_url" not in model.keys():
-            continue
-        normalized_path = model['name'].rstrip("/")
-        file_name = os.path.join(gpu_info_path, os.path.basename(normalized_path) + f"_{timestamp}.txt")
-        if "gpu_interval" not in model.keys():
-            interval = 3
-        else:
-            interval = model['gpu_interval']
-        tasks.append(
-            monitor_gpu(
-                model['gpu_url'] + "/gpu_info",
-                interval=interval,
-                file_name=file_name,
-                stop_event=stop_event
-            )
-        )
-
-    await asyncio.gather(*tasks)
+from utils.file_helper import *
+from utils.gpu_monitor import *
+from utils.summary import *
 
 
 # RUNNING RELATED
@@ -193,105 +140,6 @@ async def combined_run(load_path, file_list, models, save_path, eval_dict, save_
     await main(load_path, file_list, models, save_path, save_response, eval_dict)
     stop_event.set()
     await gpu_task
-
-
-# EVALUATION RELATED
-def model_summary_table(
-    eval_dict, save_path
-):  # get the ealiest start time, the latest end time, total_prompt_num, total_decode_num, total_decode_speed for every model
-    model_summary = {}
-
-    for file, model_list in eval_dict.items():
-        for record in model_list:
-            model_name = record["model"]
-            if record["start_time"] == -1:
-                continue
-            start_time = datetime.fromisoformat(record["start_time"])
-            end_time = datetime.fromisoformat(record["end_time"])
-
-            if model_name not in model_summary:
-                model_summary[model_name] = {
-                    "earliest_start": start_time,
-                    "latest_end": end_time,
-                    "total_prompt_num": record['prompt_token_len'],
-                    "total_decode_num": record['decode_token_len'],
-                    'latest_start': start_time,
-                    'earliest_end': end_time
-                }
-            else:
-                model_summary[model_name]["earliest_start"] = min(
-                    model_summary[model_name]["earliest_start"], start_time
-                )
-                model_summary[model_name]["latest_end"] = max(model_summary[model_name]["latest_end"], end_time)
-                # model_summary[model_name]["earliest_end"] = min(model_summary[model_name]["earliest_end"], end_time)
-                # model_summary[model_name]["latest_start"] = max(model_summary[model_name]["latest_start"], start_time)
-                model_summary[model_name]['total_prompt_num'] += record['prompt_token_len']
-                model_summary[model_name]['total_decode_num'] += record['decode_token_len']
-
-    for model_name, summary_item in model_summary.items():
-        summary_item['total_runtime'] = (summary_item['latest_end'] - summary_item['earliest_start']).total_seconds()
-        summary_item['decode_speed'] = summary_item['total_decode_num'] / summary_item['total_runtime'] if summary_item[
-            "total_runtime"] > 0 else -1
-        # print(model_name, summary_item['earliest_start'], summary_item['latest_start'], summary_item['earliest_end'], summary_item['latest_end'])
-
-    data = []
-    for model_name, summary_item in model_summary.items():
-        data.append(
-            {
-                "Model": model_name,
-                "Total Prompt Tokens": summary_item["total_prompt_num"],
-                "Total Decode Tokens": summary_item["total_decode_num"],
-                "Total Runtime (s)": round(summary_item["total_runtime"], 2),
-                "Decode Speed (Tokens / s)": round(summary_item["decode_speed"], 2)
-                if summary_item["decode_speed"] != -1 else -1
-            }
-        )
-
-    df = pd.DataFrame(data)
-
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_name = f"model_summary_table_{timestamp}.xlsx"
-    output_file_path = os.path.join(save_path, file_name)
-
-    df.to_excel(output_file_path, index=False)
-
-
-def file_summary_table(eval_dict, save_path):
-    data = []
-    for prompt, entries in eval_dict.items():
-        for entry in entries:
-            data.append(
-                {
-                    'Prompt': prompt,
-                    'Model': entry['model'],
-                    'Prompt Token Length': entry['prompt_token_len'],
-                    'Decode Token Length': entry['decode_token_len'],
-                    'Elapsed Time(s)': entry['elapsed_time']
-                }
-            )
-
-    df = pd.DataFrame(data)
-
-    df['Decode Speed(Token / s)'] = df.apply(
-        lambda row: round(row['Decode Token Length'] / row['Elapsed Time(s)'], 2)
-        if row['Decode Token Length'] != -1 and row['Elapsed Time(s)'] > 0 else -1,
-        axis=1
-    )
-
-    df['Elapsed Time(s)'] = df['Elapsed Time(s)'].apply(lambda x: round(x, 3) if x >= 0 else x)
-
-    df_display = df.copy()
-    df_display.loc[df_display.duplicated(subset=['Prompt']), 'Prompt'] = ''
-
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_name = f"file_summary_table_{timestamp}.xlsx"
-
-    output_file_path = os.path.join(save_path, file_name)
-    df_display.to_excel(output_file_path, index=False)
-
-
-def evaluate(eval_dict):
-    return NotImplementedError
 
 
 if __name__ == "__main__":
